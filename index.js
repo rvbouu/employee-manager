2160ce84c038720612666818858d8079d0936cf3
Vanessa Bou <rvbouu@gmail.com>
2024-05-03 16:08:33 -0500

    exported modules from questions.js and created switch statement in index.js

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
-
-const pg = require('pg');
+const { Pool } = require('pg');
 const colors = require('colors');
+const prompt = require('./lib/questions');
+
+
+const connection = new Pool(
+  {
+    //PostgreSQL username
+    user: 'rvbou',
+    //PostgreSQL password
+    password: 'redrum',
+    host: 'localhost',
+    database: 'employees_db'
+  },
+)
+
+connection.connect(err => {
+  if (err) throw err;
+  afterConnect();
+})
+
+// Welcome image function that displays after connection is made
+afterConnect = () => {
+  console.log(".-----------------------------------------------------.".yellow)
+  console.log("|   ".yellow + " _____                 _                          ".magenta + "|".yellow)
+  console.log("|   ".yellow + "| ____|_ __ ___  _ __ | | ___  _   _  ___  ___    ".magenta + "|".yellow)
+  console.log("|   ".yellow + "|  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\   ".magenta + "|".yellow)
+  console.log("|   ".yellow + "| |___| | | | | | |_) | | (_) | |_| |  __/  __/   ".magenta + "|".yellow)
+  console.log("|   ".yellow + "|_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|   ".magenta + "|".yellow)
+  console.log("|   ".yellow + " __  __         |_|            |___/              ".magenta + "|".yellow)
+  console.log("|   ".yellow + "|  \\/  | __ _ _ __   __ _  __ _  ___ _ __         ".magenta + "|".yellow)
+  console.log("|   ".yellow + "| |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|        ".magenta + "|".yellow)
+  console.log("|   ".yellow + "| |  | | (_| | | | | (_| | (_| |  __/ |           ".magenta + "|".yellow)
+  console.log("|   ".yellow + "|_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           ".magenta + "|".yellow)
+  console.log("|   ".yellow + "                          |___/                   ".magenta + "|".yellow)
+  console.log("'-----------------------------------------------------'".yellow)
+
+  prompt.mainPrompt().then((data) => {
+    switch (data) {
+      case 'View All Employees':
+        showAllEmployees();
+        break;
+      case 'Add Employee':
+        addNewEmployee();
+        break;
+      case 'Update Employee Role':
+        updateEmpRole();
+        break;
+      case 'View All Roles':
+        showAllRoles();
+        break;
+      case 'Add Role':
+        addNewRole();
+        break;
+      case 'View All Departments':
+        showAllDepts();
+        break;
+      case 'Add Department':
+        addNewDept();
+        break;
+      case 'Quit':
+        connection.end();
+        break;
+    }
+  })
+}
 
 
 
@@ -10,25 +73,3 @@ const colors = require('colors');
 
 
 
-
-
-
-
-
-
-
-
-
-console.log(".-----------------------------------------------------.".yellow)
-console.log("|   ".yellow+" _____                 _                          ".magenta+"|".yellow)
-console.log("|   ".yellow+"| ____|_ __ ___  _ __ | | ___  _   _  ___  ___    ".magenta+"|".yellow)
-console.log("|   ".yellow+"|  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\   ".magenta+"|".yellow)
-console.log("|   ".yellow+"| |___| | | | | | |_) | | (_) | |_| |  __/  __/   ".magenta+"|".yellow)
-console.log("|   ".yellow+"|_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|   ".magenta+"|".yellow)
-console.log("|   ".yellow+" __  __         |_|            |___/              ".magenta+"|".yellow)
-console.log("|   ".yellow+"|  \\/  | __ _ _ __   __ _  __ _  ___ _ __         ".magenta+"|".yellow)
-console.log("|   ".yellow+"| |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|        ".magenta+"|".yellow)
-console.log("|   ".yellow+"| |  | | (_| | | | | (_| | (_| |  __/ |           ".magenta+"|".yellow)
-console.log("|   ".yellow+"|_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           ".magenta+"|".yellow)
-console.log("|   ".yellow+"                          |___/                   ".magenta+"|".yellow)
-console.log("'-----------------------------------------------------'".yellow)
