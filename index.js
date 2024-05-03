61433843e19b4d1b2001110d9966c2292c329bea
Vanessa Bou <rvbouu@gmail.com>
2024-05-03 17:27:41 -0500

    trying to get table to display correctly in console

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
+const inquirer = require('inquirer');
 const colors = require('colors');
 const prompt = require('./lib/questions');
+const consTable = require('console.table');
 
 
 const connection = new Pool(
@@ -34,38 +36,63 @@ afterConnect = () => {
   console.log("|   ".yellow + "|_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           ".magenta + "|".yellow)
   console.log("|   ".yellow + "                          |___/                   ".magenta + "|".yellow)
   console.log("'-----------------------------------------------------'".yellow)
-
-  prompt.mainPrompt().then((data) => {
-    switch (data) {
-      case 'View All Employees':
-        showAllEmployees();
-        break;
-      case 'Add Employee':
-        addNewEmployee();
-        break;
-      case 'Update Employee Role':
-        updateEmpRole();
-        break;
-      case 'View All Roles':
-        showAllRoles();
-        break;
-      case 'Add Role':
-        addNewRole();
-        break;
-      case 'View All Departments':
-        showAllDepts();
-        break;
-      case 'Add Department':
-        addNewDept();
-        break;
-      case 'Quit':
-        connection.end();
-        break;
-    }
-  })
+  mainPrompt();
 }
 
+const mainPrompt = () => inquirer.prompt([
+  {
+    type: 'list',
+    message: 'What would you like to do?',
+    name: 'mainMenu',
+    choices: [
+      'View All Employees',
+      'Add Employee',
+      'Update Employee Role',
+      'View All Roles',
+      'Add Role',
+      'View All Departments',
+      'Add Department',
+      'Quit']
+  }
+]).then((ans) => {
+  const {mainMenu} = ans;
+  switch (mainMenu) {
+    case 'View All Employees':
+      showAllEmployees();
+      break;
+    case 'Add Employee':
+      addNewEmployee();
+      break;
+    case 'Update Employee Role':
+      updateEmpRole();
+      break;
+    case 'View All Roles':
+      showAllRoles();
+      break;
+    case 'Add Role':
+      addNewRole();
+      break;
+    case 'View All Departments':
+      showAllDepts();
+      break;
+    case 'Add Department':
+      addNewDept();
+      break;
+    case 'Quit':
+      connection.end();
+      break;
+  }
+})
 
+showAllDepts = () => {
+  console.log('Showing all departments...\n');
+  const sql = 'SELECT department.id AS id, department.name AS department FROM department'
+  connection.promise().query(sql, (err, rows) => {
+    if(err) throw err;
+    console.table(rows);
+    mainPrompt();
+  })
+}
 
 
 
