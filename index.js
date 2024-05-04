bf1d3ecaaf71d55c0a60909ee58f736a224db613
Vanessa Bou <rvbouu@gmail.com>
2024-05-04 00:04:01 -0500

    added showAllRoles, showAllEmployees, and addNewEmployee functions to index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const prompt = require('./lib/questions');
 const consTable = require('console.table');
 
 
-const connection = new Pool(
+const pool = new Pool(
   {
     //PostgreSQL username
     user: 'rvbou',
@@ -15,29 +15,6 @@ const connection = new Pool(
     database: 'employees_db'
   },
 )
-
-connection.connect(err => {
-  if (err) throw err;
-  afterConnect();
-})
-
-// Welcome image function that displays after connection is made
-afterConnect = () => {
-  console.log(".-----------------------------------------------------.".yellow)
-  console.log("|   ".yellow + " _____                 _                          ".magenta + "|".yellow)
-  console.log("|   ".yellow + "| ____|_ __ ___  _ __ | | ___  _   _  ___  ___    ".magenta + "|".yellow)
-  console.log("|   ".yellow + "|  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\   ".magenta + "|".yellow)
-  console.log("|   ".yellow + "| |___| | | | | | |_) | | (_) | |_| |  __/  __/   ".magenta + "|".yellow)
-  console.log("|   ".yellow + "|_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|   ".magenta + "|".yellow)
-  console.log("|   ".yellow + " __  __         |_|            |___/              ".magenta + "|".yellow)
-  console.log("|   ".yellow + "|  \\/  | __ _ _ __   __ _  __ _  ___ _ __         ".magenta + "|".yellow)
-  console.log("|   ".yellow + "| |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|        ".magenta + "|".yellow)
-  console.log("|   ".yellow + "| |  | | (_| | | | | (_| | (_| |  __/ |           ".magenta + "|".yellow)
-  console.log("|   ".yellow + "|_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           ".magenta + "|".yellow)
-  console.log("|   ".yellow + "                          |___/                   ".magenta + "|".yellow)
-  console.log("'-----------------------------------------------------'".yellow)
-  mainPrompt();
-}
 
 const mainPrompt = () => inquirer.prompt([
   {
@@ -55,7 +32,7 @@ const mainPrompt = () => inquirer.prompt([
       'Quit']
   }
 ]).then((ans) => {
-  const {mainMenu} = ans;
+  const { mainMenu } = ans;
   switch (mainMenu) {
     case 'View All Employees':
       showAllEmployees();
@@ -79,20 +56,190 @@ const mainPrompt = () => inquirer.prompt([
       addNewDept();
       break;
     case 'Quit':
-      connection.end();
+
+      pool.end();
       break;
   }
 })
 
-showAllDepts = () => {
-  console.log('Showing all departments...\n');
-  const sql = 'SELECT department.id AS id, department.name AS department FROM department'
-  connection.promise().query(sql, (err, rows) => {
-    if(err) throw err;
-    console.table(rows);
+showAllEmployees = () => {
+  console.log('Showing all employees...\n');
+  const sql = `SELECT employee.id,
+  employee.first_name,
+  employee.last_name,
+  roles.title,
+  department.name AS department,
+  roles.salary,
+  CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN roles ON employee.role_id = roles.id
+  LEFT JOIN department ON roles.department_id = department.id
+  LEFT JOIN employee manager ON employee.manager_id = manager.id`
+  pool.query(sql, (err, results) => {
+    if (err) throw err;
+    console.table(results.rows);
     mainPrompt();
   })
 }
+
+showAllRoles = () => {
+  console.log('Showing all departments...\n');
+  const sql = `SELECT roles.id,
+  roles.title,
+  department.name AS department,
+  roles.salary
+  FROM roles
+  JOIN department ON roles.department_id = department.id`
+  pool.query(sql, (err, results) => {
+    if (err) throw err;
+    console.table(results.rows);
+    mainPrompt();
+  })
+}
+
+showAllDepts = () => {
+  console.log('Showing all departments...\n');
+  const sql = `SELECT * FROM department`
+  pool.query(sql, (err, results) => {
+    if (err) throw err;
+    console.table(results.rows);
+    mainPrompt();
+  })
+}
+
+addNewEmployee = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: "What is the employee's first name?",
+      name: 'firstName',
+      validate: addFName => {
+        if (addFName) {
+          return true;
+        } else {
+          console.log("Please enter the employee's first name: ");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "What is the employee's last name?",
+      name: 'lastName',
+      validate: addLName => {
+        if (addLName) {
+          return true;
+        } else {
+          console.log("Please enter the employee's last name: ");
+          return false;
+        }
+      }
+    }
+  ]).then((ans) => {
+    const params = [ans.firstName, ans.lastName];
+
+    const roleSql = `SELECT roles.id, roles.title FROM roles`;
+    pool.query(roleSql, (err, data) => {
+      if (err) throw err;
+      const roles = data.rows.map(({ id, title }) => ({ name: title, value: id }));
+
+      inquirer.prompt([
+        {
+          type: 'list',
+          message: `What is the employee's role?`,
+          name: 'role',
+          choices: roles
+        }
+      ]).then(roleAns => {
+        const role = roleAns.role;
+        params.push(role);
+
+        const managerSql = `SELECT * FROM employee`;
+        pool.query(managerSql, (err, data) => {
+          if (err) throw err;
+          console.log(data.rows)
+          const managers = data.rows.map(({ id, first_name, last_name }) => ({ name: first_name + ' ' + last_name, value: id }));
+
+          inquirer.prompt([
+            {
+              type: 'list',
+              message: `Who is the employee's manager?`,
+              name: 'manager',
+              choices: managers
+            }
+          ]).then(managerAns => {
+            const manager = managerAns.manager;
+            params.push(manager);
+
+            const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES ($1, $2, $3, $4)`;
+            console.log(params)
+            pool.query(sql, params, (err, results) => {
+              if (err) throw err;
+              console.log(`Added ${ans.firstName} ${ans.lastName} to the database.\n`);
+
+              mainPrompt();
+            })
+          })
+        })
+      })
+    })
+  })
+}
+
+// Welcome image function that displays after connection is made
+afterConnect = () => {
+      console.log(".-----------------------------------------------------.".yellow)
+      console.log("|   ".yellow + " _____                 _                          ".magenta + "|".yellow)
+      console.log("|   ".yellow + "| ____|_ __ ___  _ __ | | ___  _   _  ___  ___    ".magenta + "|".yellow)
+      console.log("|   ".yellow + "|  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\   ".magenta + "|".yellow)
+      console.log("|   ".yellow + "| |___| | | | | | |_) | | (_) | |_| |  __/  __/   ".magenta + "|".yellow)
+      console.log("|   ".yellow + "|_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|   ".magenta + "|".yellow)
+      console.log("|   ".yellow + " __  __         |_|            |___/              ".magenta + "|".yellow)
+      console.log("|   ".yellow + "|  \\/  | __ _ _ __   __ _  __ _  ___ _ __         ".magenta + "|".yellow)
+      console.log("|   ".yellow + "| |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|        ".magenta + "|".yellow)
+      console.log("|   ".yellow + "| |  | | (_| | | | | (_| | (_| |  __/ |           ".magenta + "|".yellow)
+      console.log("|   ".yellow + "|_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           ".magenta + "|".yellow)
+      console.log("|   ".yellow + "                          |___/                   ".magenta + "|".yellow)
+      console.log("'-----------------------------------------------------'".yellow)
+      mainPrompt();
+    }
+
+afterConnect();
+
+
+
+
+
+//   switch (mainMenu) {
+//     case 'View All Employees':
+//       showAllEmployees();
+//       break;
+//     case 'Add Employee':
+//       addNewEmployee();
+//       break;
+//     case 'Update Employee Role':
+//       updateEmpRole();
+//       break;
+//     case 'View All Roles':
+//       showAllRoles();
+//       break;
+//     case 'Add Role':
+//       addNewRole();
+//       break;
+//     case 'View All Departments':
+//       showAllDepts();
+//       break;
+//     case 'Add Department':
+//       addNewDept();
+//       break;
+//     case 'Quit':
+
+//       pool.end();
+//       break;
+//   }
+
+
 
 
 
