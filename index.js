84726cf1b68d7d5381b3f9be7a0c240bf3ec826d
Vanessa Bou <rvbouu@gmail.com>
2024-05-05 15:52:54 -0500

    created updateEmpRole function in update.js and connected add.js to questions.js

--- index.js
+++ index.js
@@ -281,54 +281,54 @@ const consTable = require('console.table');
 //   })
 // }
 
-/* ---------- Functions to update items in tables ---------- */
-// Updates employee's role
-updateEmpRole = () => {
-  const empSql = `SELECT * FROM employee`;
-  pool.query(empSql, (err, data) => {
-    if (err) throw err;
-    const employees = data.rows.map(({id, first_name, last_name}) => ({name: first_name+' '+last_name, value: id}));
+// /* ---------- Functions to update items in tables ---------- */
+// // Updates employee's role
+// updateEmpRole = () => {
+//   const empSql = `SELECT * FROM employee`;
+//   pool.query(empSql, (err, data) => {
+//     if (err) throw err;
+//     const employees = data.rows.map(({id, first_name, last_name}) => ({name: first_name+' '+last_name, value: id}));
 
-    inquirer.prompt([
-      {
-        type: 'list',
-        message: `Which employee's role do you want to update?`,
-        name: 'emp',
-        choices: employees
-      }
-    ]).then((ans) => {
-      const params = [ans.emp];
+//     inquirer.prompt([
+//       {
+//         type: 'list',
+//         message: `Which employee's role do you want to update?`,
+//         name: 'emp',
+//         choices: employees
+//       }
+//     ]).then((ans) => {
+//       const params = [ans.emp];
 
-      const roleSql = `SELECT * FROM roles`;
-      pool.query(roleSql, (err, data) => {
-        if (err) throw err;
-        const roles = data.rows.map(({id, title}) => ({name: title, value: id}));
+//       const roleSql = `SELECT * FROM roles`;
+//       pool.query(roleSql, (err, data) => {
+//         if (err) throw err;
+//         const roles = data.rows.map(({id, title}) => ({name: title, value: id}));
 
-        inquirer.prompt([
-          {
-            type: 'list',
-            message: `Which role do you want to assign the selected employee?`,
-            name: 'role',
-            choices: roles
-          }
-        ]).then(roleAns => {
-          const role = roleAns.role;
-          params.push(role);
+//         inquirer.prompt([
+//           {
+//             type: 'list',
+//             message: `Which role do you want to assign the selected employee?`,
+//             name: 'role',
+//             choices: roles
+//           }
+//         ]).then(roleAns => {
+//           const role = roleAns.role;
+//           params.push(role);
 
-          const sql = `UPDATE employee SET role_id = $2 WHERE id = $1`;
+//           const sql = `UPDATE employee SET role_id = $2 WHERE id = $1`;
 
-          pool.query(sql, params, (err, results) => {
-            if(err) throw err;
-            console.log(`\nUpdated employee's role.\n`.green);
-            mainPrompt();
-          })
-        })
-      })
-    })
-  })
-}
+//           pool.query(sql, params, (err, results) => {
+//             if(err) throw err;
+//             console.log(`\nUpdated employee's role.\n`.green);
+//             mainPrompt();
+//           })
+//         })
+//       })
+//     })
+//   })
+// }
 
-// TODO: Updates employee's manager
+// // TODO: Updates employee's manager
 
 /* ---------- Functions to delete items from table ---------- */
 // TODO: Deletes employee
