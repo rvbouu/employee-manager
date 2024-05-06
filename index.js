16da6400315cd24c7f348d08cf509e10215c3b9a
Vanessa Bou <rvbouu@gmail.com>
2024-05-06 00:57:16 -0500

    added comments to index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+// declaring dependencies and variables
 const prompt = require('./lib/questions');
 const colors = require('colors');
 
 // Welcome image function that displays after connection is made
 afterConnect = () => {
-      console.log(".-----------------------------------------------------.".yellow)
-      console.log("|   ".yellow + " _____                 _                          ".magenta + "|".yellow)
-      console.log("|   ".yellow + "| ____|_ __ ___  _ __ | | ___  _   _  ___  ___    ".magenta + "|".yellow)
-      console.log("|   ".yellow + "|  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\   ".magenta + "|".yellow)
-      console.log("|   ".yellow + "| |___| | | | | | |_) | | (_) | |_| |  __/  __/   ".magenta + "|".yellow)
-      console.log("|   ".yellow + "|_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|   ".magenta + "|".yellow)
-      console.log("|   ".yellow + " __  __         |_|            |___/              ".magenta + "|".yellow)
-      console.log("|   ".yellow + "|  \\/  | __ _ _ __   __ _  __ _  ___ _ __         ".magenta + "|".yellow)
-      console.log("|   ".yellow + "| |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|        ".magenta + "|".yellow)
-      console.log("|   ".yellow + "| |  | | (_| | | | | (_| | (_| |  __/ |           ".magenta + "|".yellow)
-      console.log("|   ".yellow + "|_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           ".magenta + "|".yellow)
-      console.log("|   ".yellow + "                          |___/                   ".magenta + "|".yellow)
-      console.log('|                                                     |'.yellow)
-      console.log("'-----------------------------------------------------'\n".yellow)
-      prompt.mainPrompt();
-    }
+  console.log(".-----------------------------------------------------.".yellow);
+  console.log("|   ".yellow + " _____                 _                          ".magenta + "|".yellow);
+  console.log("|   ".yellow + "| ____|_ __ ___  _ __ | | ___  _   _  ___  ___    ".magenta + "|".yellow);
+  console.log("|   ".yellow + "|  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\   ".magenta + "|".yellow);
+  console.log("|   ".yellow + "| |___| | | | | | |_) | | (_) | |_| |  __/  __/   ".magenta + "|".yellow);
+  console.log("|   ".yellow + "|_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|   ".magenta + "|".yellow);
+  console.log("|   ".yellow + " __  __         |_|            |___/              ".magenta + "|".yellow);
+  console.log("|   ".yellow + "|  \\/  | __ _ _ __   __ _  __ _  ___ _ __         ".magenta + "|".yellow);
+  console.log("|   ".yellow + "| |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|        ".magenta + "|".yellow);
+  console.log("|   ".yellow + "| |  | | (_| | | | | (_| | (_| |  __/ |           ".magenta + "|".yellow);
+  console.log("|   ".yellow + "|_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|           ".magenta + "|".yellow);
+  console.log("|   ".yellow + "                          |___/                   ".magenta + "|".yellow);
+  console.log('|                                                     |'.yellow);
+  console.log("'-----------------------------------------------------'\n".yellow);
+  // calls mainPrompt function after displaying welcome image
+  prompt.mainPrompt();
+}
 
+// initiates start of application
 afterConnect();
 
